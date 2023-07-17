54256be6e65ce19790b7baf50012235fd6a7d37a
Amiyal Mavani <swetamavani22@gmail.com>
2023-07-17 21:35:22 +0530

    First Commite - Create Project

---
+++ PropeliusTask/Controller/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  PropeliusTask
+//
+//  Created by Mavani on 17/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - Outlets
+
+    // MARK: - Variable Decleration
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.SetUp()
+    }
+
+    // MARK: - Private Method
+    func SetUp() {
+        
+    }
+
+    // MARK: - Button Action
+}
+
